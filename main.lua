54cf4a2d44ff4991e11bbbfb01e1143bd6242727
ZaynHub1 <budialamzyah79@gmail.com>
2026-07-19 21:14:27 +0700

    Update and rename Zay Hub to main.lua

---
+++ main.lua
@@ -0,0 +1,11 @@
+--!strict
+-- [ ZAYN HUB SECURE LOADER ] --
+-- Source code asli aman tersembunyi.
+
+local sukses, err = pcall(function()
+    loadstring(game:HttpGet("https://gist.githubusercontent.com/ZaynHub1/2f93bc296c437c437c4d69dee262a2d231c6/raw/8a834d4ec195548b9c786a38e79d35893c97cf46/Zay%2520Hub", true))()
+end)
+
+if not sukses then
+    warn("Zayn Hub Secure Loader Error: " .. tostring(err))
+end
